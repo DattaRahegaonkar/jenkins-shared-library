dd72cd88d4de3a22d2c59dda3914f917fd4d0b64
Datta Rahegaonkar <163870608+DattaRahegaonkar@users.noreply.github.com>
2025-09-10 03:15:44 +0530

    Update trivyimagescan.groovy

--- vars/trivyimagescan.groovy
+++ vars/trivyimagescan.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String reportName, String tagName) {
-  def reportNameNew = "${reportName}-${buildNumber}.html"
+  def reportNameNew = "${reportName}-${tagName}.html"
   sh "trivy image ${imageName}:${tagName} --format table -o ${reportNameNew} --exit-code 0 --severity HIGH,CRITICAL || true"
 
   archiveArtifacts artifacts: reportName, allowEmptyArchive: true
